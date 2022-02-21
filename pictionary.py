704206f9b505f2af3d93020f7344d16e60ffec0a
AKROCKS12 <77364723+AKROCKS12@users.noreply.github.com>
2022-02-21 15:56:52 -0500

    Debug#1
    fixed issue where the program was not able to randomly select a value in a nested list

--- pictionary.py
+++ pictionary.py
@@ -1,3 +1,4 @@
+from ntpath import join
 from pydoc_data.topics import topics
 import random
 from random import randint
@@ -15,42 +16,41 @@ animals = ["Dog", "Cat", "Tiger", "Lion",  "Giraffe", "Elephant", "Fish", "Bear"
 fruits =  ["Apple", "Banana", "Orange", "Strawberry", "Blueberry", "Raspberry", "Blackberry", "Mango", "Avocado", "Kiwi", "Pineapple", "Watermelon", "Pear", "Grapes", "Peach",]
 countries =  ["Canada", "United States Of America", "Mexico", "Brazil", "Argentina", "Africa", "China", "Japan", "India", "Sri Lanka", "Australia", "Russia", "Iran", "New Zealand", "United Kingdom"]
 sports = ["Basketball", "Soccer", "Baseball", "Football", "Tennis", "Badminton", "Volleyball", "Hockey", "Cricket", "Golf", "Curling", "Gymnastics", "Polo", "Cycling", "Skiing",]
-
+finaltopiclist = []
 topic2 = []
 finaltopiclist = [animals, fruits, countries, sports]
 def randomize():
-     print("working")
-     random_topic = random.sample(finaltopiclist, 1)
-     print(random_topic)
-     final_word = random.choice(random_topic)
+     random_topic = []
+     random_topic = random.choice(finaltopiclist)
+     final_word = random.sample(random_topic,1)
+     print(final_word)
      topic2.append(final_word)
-     print(topic2)
-randomize()
+#randomize()
 
-#def askThread(queue):
- # question = "The word is <something> if you got it correct press anything\n"
-  #value = input(question)
-  #queue.put(value)
-#
-#def main():
-#    q = queue.Queue()
-#    while True:
-#        answer = input("Ask question? \n")
-#        if answer == 'y':
-#            thread = threading.Thread(target=askThread, args=(q,))
-#            thread.start()
-#            i = 60
-#            while i > 0:
-#                print("Time remaining" + str(i))
-#                time.sleep(1)
-#                i=i-1
-#                if(not q.empty()):
-#                  val=q.get()
-#                  print(val)
-#                  break
-#            print("user pressed in " + str(i) + 'secs')  
-#        if answer == 'n':
-#          print("thanks for playing!")
-#          break
-#if __name__ == '__main__':
-#  main()
+def askThread(queue):
+     question = "The word is" + randomize() + "if you got it correct press anything\n"
+     value = input(question)
+     queue.put(value)
+
+def main():
+    q = queue.Queue()
+    while True:
+        answer = input("Ask question? \n")
+        if answer == 'yes':
+            thread = threading.Thread(target=askThread, args=(q,))
+            thread.start()
+            i = 60
+            while i > 0:
+                print("Time remaining" + str(i))
+                time.sleep(1)
+                i=i-1
+                if(not q.empty()):
+                  val=q.get()
+                  print(val)
+                  break
+            print("user pressed in " + str(i) + 'secs')  
+        if answer == 'no':
+          print("thanks for playing!")
+          break
+if __name__ == '__main__':
+  main()
