fe406bab31391e9330af3614b469722885dcdd28
AKROCKS12 <77364723+AKROCKS12@users.noreply.github.com>
2022-02-28 20:35:17 -0500

    Update pictionary.py

--- pictionary.py
+++ pictionary.py
@@ -1,17 +1,9 @@
-from ntpath import join
-from pydoc_data.topics import topics
-import random
-from random import randint
 import queue
+from re import U
 import threading, time
-
-
-
-#animals = ["Dog", "Cat", "Tiger", "Lion",  "Giraffe", "Elephant", "Fish", "Bear", "Rabbit", "Kangaroo", "Penguin",   "Rat", "Bird", "Hamster", "Squirrel"]#15 topics
-#fruits = ["Apple", "Banana", "Orange", "Strawberry", "Blueberry", "Raspberry", "Blackberry", "Mango", "Avocado", "Kiwi", "Pineapple", "Watermelon", "Pear", "Grapes", "Peach",]#15 topics
-#countries = ["Canada", "United States Of America", "Mexico", "Brazil", "Argentina", "Africa", "China", "Japan", "India", "Sri Lanka", "Australia", "Russia", "Iran", "New Zealand", "United Kingdom"]#15 topics
-#sports =["Basketball", "Soccer", "Baseball", "Football", "Tennis", "Badminton", "Volleyball", "Hockey", "Cricket", "Golf", "Curling", "Gymnastics", "Polo", "Cycling", "Skiing",] #15 topics
-
+import random
+import sys
+import os
 animals = ["Dog", "Cat", "Tiger", "Lion",  "Giraffe", "Elephant", "Fish", "Bear", "Rabbit", "Kangaroo", "Penguin",   "Rat", "Bird", "Hamster", "Squirrel"]
 fruits =  ["Apple", "Banana", "Orange", "Strawberry", "Blueberry", "Raspberry", "Blackberry", "Mango", "Avocado", "Kiwi", "Pineapple", "Watermelon", "Pear", "Grapes", "Peach",]
 countries =  ["Canada", "United States Of America", "Mexico", "Brazil", "Argentina", "Africa", "China", "Japan", "India", "Sri Lanka", "Australia", "Russia", "Iran", "New Zealand", "United Kingdom"]
@@ -19,38 +11,43 @@ sports = ["Basketball", "Soccer", "Baseball", "Football", "Tennis", "Badminton",
 finaltopiclist = []
 topic2 = []
 finaltopiclist = [animals, fruits, countries, sports]
+
 def randomize():
      random_topic = []
      random_topic = random.choice(finaltopiclist)
      final_word = random.sample(random_topic,1)
-     print(final_word)
+     #print(final_word)
      topic2.append(final_word)
-#randomize()
+     return final_word
 
 def askThread(queue):
-     question = "The word is" + randomize() + "if you got it correct press anything\n"
-     value = input(question)
-     queue.put(value)
+  Randomizer = randomize()
+  question = "The word is " +" "  + Randomizer[0] + " " + " if you got it correct press anything\n"
+  value = input(question)
+  queue.put(value)
 
 def main():
-    q = queue.Queue()
-    while True:
-        answer = input("Ask question? \n")
-        if answer == 'yes':
-            thread = threading.Thread(target=askThread, args=(q,))
-            thread.start()
-            i = 60
-            while i > 0:
-                print("Time remaining" + str(i))
-                time.sleep(1)
-                i=i-1
-                if(not q.empty()):
-                  val=q.get()
-                  print(val)
-                  break
-            print("user pressed in " + str(i) + 'secs')  
-        if answer == 'no':
-          print("thanks for playing!")
-          break
+  q = queue.Queue()
+  while True:
+      answer = input("Answer a question? \n")
+      if answer == 'y':
+        thread = threading.Thread(target=askThread, args=(q,))
+        thread.start()
+        i = 60
+        while i > 0:
+          print("Time remaining " + str(i), end="\r")
+          time.sleep(1)
+          i=i-1
+          if(not q.empty()):
+            val=q.get()
+            print(val)
+            break
+      print("User pressed in " + str(i) + " secs", end ="\n")
+    
+      if answer == 'n':
+        print("thanks for playing")
+        break
+
+
 if __name__ == '__main__':
   main()
